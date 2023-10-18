e436da7ec78eca32a6b2533e8f98140040eb6240
Suiphobie <93397145+Suiphobie@users.noreply.github.com>
2023-10-18 10:55:26 +0200

    first try

--- main.py
+++ main.py
@@ -1,38 +1,25 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
 
-"""
-# Welcome to Streamlit!
+# Chargement des données
+@st.cache
+def load_data():
+    return pd.read_csv("E-SPORT ET JO - Sheet1.csv")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+data = load_data()
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Fonction pour créer un graphique en camembert
+def plot_pie_chart(column_name, title):
+    counts = data[column_name].value_counts()
+    plt.figure(figsize=(8,6))
+    plt.pie(counts, labels=counts.index, autopct='%1.1f%%', startangle=90, colors=plt.cm.Paired.colors)
+    plt.title(title)
+    return plt
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# Titre de l'application
+st.title("Résultats du sondage sur l'e-sport et les J.O.")
 
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# Affichage des graphiques
+st.pyplot(plot_pie_chart("À quelle tranche d'âge appartenez-vous ? ", "Répartition par tranche d'âge"))
+st.pyplot(plot_pie_chart("As-tu entendu parler de la semaine \"The Olympic Esports Series 2023\" ?", "Connaissance de 'The Olympic Esports Series 2023'"))
