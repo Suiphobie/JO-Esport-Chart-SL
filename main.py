5ff9dbf33746d4848f8ebccbe9e343b4dcdae6a6
Suiphobie <93397145+Suiphobie@users.noreply.github.com>
2023-10-18 11:05:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+import altair as alt
 
 # Chargement des données
 @st.cache
@@ -9,17 +9,24 @@ def load_data():
 
 data = load_data()
 
-# Fonction pour créer un graphique en camembert
-def plot_pie_chart(column_name, title):
-    counts = data[column_name].value_counts()
-    plt.figure(figsize=(8,6))
-    plt.pie(counts, labels=counts.index, autopct='%1.1f%%', startangle=90, colors=plt.cm.Paired.colors)
-    plt.title(title)
-    return plt
+# Fonction pour créer un graphique en camembert interactif avec Altair
+def interactive_pie_chart(column_name, title):
+    counts = data[column_name].value_counts().reset_index()
+    counts.columns = ['Category', 'Count']
+
+    chart = alt.Chart(counts).mark_arc(innerRadius=50, outerRadius=150).encode(
+        theta='Count:Q',
+        color='Category:N',
+        tooltip=['Category', 'Count']
+    ).properties(
+        title=title,
+        width=300,
+        height=300
+    )
+    return chart
 
 # Titre de l'application
 st.title("Résultats du sondage sur l'e-sport et les J.O.")
 
-# Affichage des graphiques
-st.pyplot(plot_pie_chart("À quelle tranche d'âge appartenez-vous ? ", "Répartition par tranche d'âge"))
-st.pyplot(plot_pie_chart("As-tu entendu parler de la semaine \"The Olympic Esports Series 2023\" ?", "Connaissance de 'The Olympic Esports Series 2023'"))
+# Affichage du graphique interactif
+st.altair_chart(interactive_pie_chart("As-tu entendu parler de la semaine \"The Olympic Esports Series 2023\" ?", "Connaissance de 'The Olympic Esports Series 2023'"))
