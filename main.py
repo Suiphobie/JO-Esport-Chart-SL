530e07eb53b00d48e9cd9ee16bd1d503008e1d33
Suiphobie <93397145+Suiphobie@users.noreply.github.com>
2023-10-19 11:45:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -28,11 +28,9 @@ st.title("Analyse détaillée en fonction de la tranche d'âge sélectionnée")
 
 # 1. Répartition par tranche d'âge
 def age_distribution_chart():
-    counts = data["À quelle tranche d'âge appartenez-vous ? "].value_counts().reset_index()
+    counts = data["À quelle tranche d'âge appartenez-vous ? "].value_counts().sort_index().reset_index()
     counts.columns = ['Tranche Âge', 'Nombre']
-    chart = alt.Chart(counts).mark_arc(innerRadius=50, outerRadius=150).encode(theta="Nombre:Q",
-        
-        y='Nombre:Q',
+    chart = alt.Chart(counts).mark_arc(innerRadius=50, outerRadius=150).encode(theta="Nombre:Q"
         color=alt.Color('Tranche Âge:N', legend=None),
         tooltip=['Tranche Âge', 'Nombre']
     ).properties(
