74267d2e7bea7dac50fd51e88c5e1a20a258c818
Suiphobie <93397145+Suiphobie@users.noreply.github.com>
2023-10-19 14:36:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ left_column, center_column, right_column = st.columns(3)
 
 # Affichage du graphique dans la colonne centrale
 with left_column:
-    st.write(f"Nombre de répondants: {len(data)}")
+    st.markdown(f"## Nombre de répondants: {len(data)}")
 with center_column:
     st.altair_chart(age_distribution_chart())
 
